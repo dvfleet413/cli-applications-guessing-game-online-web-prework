9b3d4895297b96735733096782654324c1f87cce
David Van Fleet <dvfleet413@gmail.com>
2019-12-24 15:28:35 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,4 +1,21 @@
 # Code your solution here!
+def generate_random_number
+  num = rand(6) + 1
+end
+
+def capture_guess
+  
+end
+
+def compare_numbers
+
+end
+
+def print_result
+  
+end
+
+
 def run_guessing_game
   
 end
