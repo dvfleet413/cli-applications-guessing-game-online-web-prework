5a41b0f59bc32c45010908dcc7a10b8d856d0ea7
David Van Fleet <dvfleet413@gmail.com>
2019-12-24 16:02:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,32 +1,15 @@
 require 'pry'
 # Code your solution here!
-def generate_random_number
-  num = rand(6) + 1
-end
-
-$number = generate_random_number
-
-def capture_guess
-  guess = gets.chomp
-end
-
-def correct_guess?(guess)
-  if guess.to_i == $number
-    return true 
-  else
-    return false 
-  end
-end
 
 def run_guessing_game
-  guess = capture_guess
-  number = num = rand(6) + 1
+  guess = gets.chomp
+  number = rand(6) + 1
   if guess == "exit"
     puts "Goodbye!"
-  elsif correct_guess?(guess)
+  elsif guess.to_i == number
     puts "You guessed the correct number!"
   else
-    puts "Sorry! The computer guessed #{$number}."
+    puts "Sorry! The computer guessed #{number}."
   end
 
 end
