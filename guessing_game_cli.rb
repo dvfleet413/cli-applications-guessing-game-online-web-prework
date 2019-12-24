e94398f4868d0dc69cf362feef84c788e6dd7281
David Van Fleet <dvfleet413@gmail.com>
2019-12-24 15:37:14 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,21 +3,25 @@ def generate_random_number
   num = rand(6) + 1
 end
 
-num = generate_random_number
+number = generate_random_number
 
 def capture_guess
+  puts "Guess a number between 1 and 6:"
   guess = gets.chomp
 end
 
-def compare_numbers
-
+def correct_guess?(guess)
+  if guess == number
+    return true 
+  else
+    return false 
+  end
 end
-
-def print_result
-  
-end
-
 
 def run_guessing_game
-  
+  capture_guess
+  if correct_guess?
+    puts "You guessed the correct number!"
+  else
+    puts ""
 end
